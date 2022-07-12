816b366bcfb9fc2419cb2949918bb132e0f69c83
Alexandre Labbé <alexandre.labbe@ensta-paris.fr>
2022-07-12 02:05:28 +0200

    feat: use english resume version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,24 @@
+<!-- eslint-disable max-len -->
 <template>
-    <div id="container">
-        <canvas id="background"></canvas>
-        <div id="overlay">
-            <h1 class="secondary--text text-md-h1 text-h3 mb-6 header-title">
-                Alexandre Labbé
-            </h1>
-            <h1 class="secondary--text text-md-h3 text-h4 header-title">
-                IT Security Consultant
-            </h1>
-            <div
-              v-for="link in contactLinks.filter(link => link.unset === false)"
-              :key="link.id"
-              class="canvas-icon"
-              :style="{'left': `${link.x}px`, 'top': `${link.y}px`}"
-            >
-              <a :href="link.url" class="text-decoration-none" :download="link.isFile && link.url">
-                <v-icon
-                  dark
-                  size="60"
-                  color="secondary"
-                >
-                  {{ link.logo }}
-                </v-icon>
-              </a>
-            </div>
-        </div>
+  <div id="container">
+    <canvas id="background"></canvas>
+    <div id="overlay">
+      <h1 class="secondary--text text-md-h1 text-h3 mb-6 header-title">
+        Alexandre Labbé
+      </h1>
+      <h1 class="secondary--text text-md-h3 text-h4 header-title">
+        IT Security Consultant
+      </h1>
+      <div v-for="link in contactLinks.filter(link => link.unset === false)" :key="link.id" class="canvas-icon"
+        :style="{ 'left': `${link.x}px`, 'top': `${link.y}px` }">
+        <a :href="link.url" class="text-decoration-none" :download="link.isFile && link.url">
+          <v-icon dark size="60" color="secondary">
+            {{ link.logo }}
+          </v-icon>
+        </a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -57,7 +50,7 @@ function getLinks(): ContactLink[] {
       y: 0,
       logo: 'mdi-file-account',
       unset: true,
-      url: 'resume_fr.pdf',
+      url: 'resume_en.pdf',
       isFile: true,
     },
   ];
@@ -65,92 +58,96 @@ function getLinks(): ContactLink[] {
 
 @Component({})
 export default class Header extends Vue {
-    canvas?: HTMLCanvasElement
+  canvas?: HTMLCanvasElement
 
-    runner?: Runner
+  runner?: Runner
 
-    contactLinks = getLinks()
+  contactLinks = getLinks()
 
-    mounted(): void {
-      this.canvas = document.getElementById('background') as HTMLCanvasElement;
-      this.runner = new Runner(
-          this.$vuetify.theme.themes.light.secondary?.toString() || '#ffffff',
-          this.$vuetify.theme.themes.light.primary?.toString() || '#ffffff',
-          this.canvas,
-          this.$device.mobile as boolean,
-      );
+  mounted(): void {
+    this.canvas = document.getElementById('background') as HTMLCanvasElement;
+    this.runner = new Runner(
+      this.$vuetify.theme.themes.light.secondary?.toString() || '#ffffff',
+      this.$vuetify.theme.themes.light.primary?.toString() || '#ffffff',
+      this.canvas,
+      this.$device.mobile as boolean,
+    );
 
-      window.addEventListener('resize', this.resizeCanvas, false);
-      this.resizeCanvas();
+    window.addEventListener('resize', this.resizeCanvas, false);
+    this.resizeCanvas();
 
-      window.setInterval(() => {
-        if (this.runner) {
-          this.runner.draw();
-        }
-      }, 8);
-    }
-
-    setContactLinks(): void {
-      this.contactLinks = (this.runner?.iconsPositions || []).map(([x, y], index) => ({
-        ...getLinks()[index],
-        x: Math.round(x),
-        y: Math.round(y),
-        unset: false,
-      }));
-    }
-
-    resizeCanvas(): void {
-      if (!this.canvas || !this.runner) {
-        return;
+    window.setInterval(() => {
+      if (this.runner) {
+        this.runner.draw();
       }
+    }, 8);
+  }
 
-      window.setTimeout(() => {
-        if (!this.canvas || !this.runner) return;
+  setContactLinks(): void {
+    this.contactLinks = (this.runner?.iconsPositions || []).map(([x, y], index) => ({
+      ...getLinks()[index],
+      x: Math.round(x),
+      y: Math.round(y),
+      unset: false,
+    }));
+  }
 
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.runner.restart();
-        this.setContactLinks();
-      }, 200);
+  resizeCanvas(): void {
+    if (!this.canvas || !this.runner) {
+      return;
     }
+
+    window.setTimeout(() => {
+      if (!this.canvas || !this.runner) return;
+
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
+      this.runner.restart();
+      this.setContactLinks();
+    }, 200);
+  }
 }
 
 </script>
 
 <style scoped>
 #container {
-    position: relative;
-    height: 100vh;
-    width: 100vw;
-    background-color: var(--v-primary-base);
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--v-primary-base);
 }
 
 #background {
-    position: absolute;
+  position: absolute;
 }
 
 #overlay {
-    position: absolute;
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
 @keyframes fadeIn {
-  0% {opacity:0;}
-  100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
 
 .header-title {
-    animation: fadeIn linear 2s;
+  animation: fadeIn linear 2s;
 }
 
 .canvas-icon {
   position: absolute;
   transform: translate(-50%, -50%);
 }
-
 </style>
